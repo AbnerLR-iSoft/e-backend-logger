8cc52054a8ff82013daf73935cbd2ef7741c0a96
Abner López <abnlopez@redialbpo.com>
2023-11-16 13:49:58 -0800

    Run project

--- src/app.js
+++ src/app.js
@@ -1,16 +1,31 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require('express')
+const logger  = require('morgan')
+require('dotenv').config() 
+const cors    = require('cors')
+const path    = require('path')
+const helmet  = require('helmet')
+const cookieParser = require('cookie-parser')
 
-const app = express();
+const app = express()
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(helmet())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(cors())
 
-app.use('/api', require('./routes/main.routes'));
+app.use('/api', require('./routes/main.routes'))
 
-module.exports = app;
+app.listen(process.env.PORT || 4000, () => {
+    console.log(`Server listeing on port ${ process.env.PORT || 4000 }`)
+  
+    // sequelize.authenticate()
+    // .then(() => {
+    //   console.log('Running Database')
+    // })
+    // .catch((err) => {
+    //   console.log(`Error connection to Database: ${err}`)
+    // })
+})
